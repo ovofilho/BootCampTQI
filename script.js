e5372c0718a3bb5909cfed7a89bce961efe28c0a
orlando oliveira <orlandofilho@gmail.com>
2022-06-01 16:22:06 -0300

    Versão Final

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
 const cartas = document.querySelectorAll('.carta');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let travaMesa = false;
+
 
 function viraCarta() {
-    this.classList.toggle('flip');
+
+    if(travaMesa) return;
+    if(this === firstCard) return;
+   
+    
+    this.classList.add('flip');// ao contrario do toggle, ele só adiciona uma vez
+    
+
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this; //recebe o elemento clicado para comparar com a segunda carta
+       
+        return;
+    }
+    secondCard = this;
+    hasFlippedCard = false; //reseta a carta selecionada
+    checkForMatch();
 }
 
-alert(cartas.length);
+function checkForMatch(){
+    
+    if(firstCard.dataset.card === secondCard.dataset.card){//exatamente iguais
 
+        disableCard();
+        return
+    }
+    desviraCarta();
+}
+
+function disableCard(){
+    firstCard.removeEventListener('click', viraCarta);
+    secondCard.removeEventListener('click', viraCarta);
+    zeraMesa();
+}
+
+function desviraCarta(){
+    travaMesa = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        zeraMesa();
+    }, 1500);
+}
+
+(function embaralhaCartas(){
+    cartas.forEach((carta)=>{
+        let posicao = Math.floor(Math.random() * 12); //sorteia um aleatorio dentro de 12 e arredonda pra baixo
+        carta.style.order = posicao;
+    });
+
+})();//imediatly invocked function, função renderizada imediatamente ao ser carregada
+
+function zeraMesa(){
+    [hasFlippedCard,travaMesa] = [false, false];
+    [firstCard, secondCard] = [null, null];
+
+}
 cartas.forEach((carta) => {
     carta.addEventListener('click',viraCarta)
 });
+
+
